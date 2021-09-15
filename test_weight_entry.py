6cdb148e143479fc494f0c35de67bc7d57106878
Hillel B. Price <hp48@duke.edu>
2021-09-15 13:03:40 -0400

    now passes pycodestyle bs

--- test_weight_entry.py
+++ test_weight_entry.py
@@ -6,13 +6,8 @@ import pytest
     ("50 kg", 50),
     ("22.1 lb", 10),
     ("22 lbs", 10),
-    #("too much", False),
-    #("22", False),
     ("22 KG", 22),
     ("22 Kg", 22),
-    #("22kg", 22),
-    #("64 g", False),
-    #("ten kg", False),
     ("-22 lb", -10)
     ])
 def test_parse_weight_input(input, expected):
